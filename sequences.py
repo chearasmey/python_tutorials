ea45a88494177cb5b52b47c7b636b914b734fb8b
Rasmey Chea <rasmeyc@KHPNH01.COM>
2021-04-24 16:57:37 +0700

    Add sequences.py

---
+++ sequences.py
@@ -0,0 +1,10 @@
+#list: sequences of mutable values
+names = ["Harry", "Jonh", "Mike"]
+#tuple: sequences of immutable values
+coordinate = (10.2, 15.5)
+#set: collection of unique value
+n = {1,2,3}
+#dict: collection of key-value pairs
+dict = {"Hello":"The greeting word", "Car":"Vehicle"}
+
+print(dict["Hello"])
