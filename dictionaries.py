b1a72d4b91caec160709a5dceae9c3da4aedad62
Rasmey Chea <rasmeyc@KHPNH01.COM>
2021-04-24 16:56:57 +0700

    Add dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,5 @@
+dict = {"Hello":"The greeting word", "Dog":"a domesticated carnivorous mammal"}
+#add item to dictionary
+dict["Name"] = "Refer to things or person"
+print(dict)
+print(dict["Hello"])
