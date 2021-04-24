1d655415babb9b89e7a9af07eb498fb9b31be295
Rasmey Chea <rasmeyc@KHPNH01.COM>
2021-04-24 16:51:05 +0700

    Add lists.py

---
+++ lists.py
@@ -0,0 +1,13 @@
+names = ["Harry", "Jonh", "Mike"]
+#add something to the end of list
+names.append("Dara")
+print(f"The list of names is {names}")
+#sort list
+names.sort()
+print(f"The list of names after sorting: {names}")
+#reverse list
+names.reverse()
+print(f"The list of names after reversing: {names}")
+#remove item from the end of list
+names.pop()
+print(f"The list of names after pop: {names}")
